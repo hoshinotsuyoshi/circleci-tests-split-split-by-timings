01ac613904db10b15936627946254f83feb94b6d
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2018-12-22 17:50:33 +0900

    Add sleep

--- spec/foo_1_spec.rb
+++ spec/foo_1_spec.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 describe 'Foo1' do
+  it do
+    sleep 0.1
+  end
 end
--- spec/foo_2_spec.rb
+++ spec/foo_2_spec.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 describe 'Foo2' do
+  it do
+    sleep 0.2
+  end
 end
